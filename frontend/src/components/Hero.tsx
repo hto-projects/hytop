b54a9aa95f07f2385b551dee610eb31e64f46d45
NightKoneko <62818210+NightKoneko@users.noreply.github.com>
2025-06-18 16:01:40 -0400

    Fix dark mode for Home and Create Project Screens

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,48 +1,94 @@
-import { Container, Paper, Title, Text, Button, Group } from "@mantine/core";
+import {
+  Container,
+  Paper,
+  Title,
+  Text,
+  Button,
+  Group,
+  Box
+} from "@mantine/core";
 import { useSelector } from "react-redux";
+import { useComputedColorScheme } from "@mantine/core";
 
 const Hero = () => {
   const primaryColor = useSelector((state: any) => state.theme.primaryColor);
+  const theColorSchemeish = useComputedColorScheme("light");
   return (
-    <Container size={600} my={80}>
-      <Paper
-        shadow="md"
-        p={40}
-        radius="md"
-        withBorder
+    <Box
+      style={{
+        minHeight: "100vh",
+        width: "100vw",
+        background: theColorSchemeish === "dark" ? "#181A1B" : undefined,
+        color: theColorSchemeish === "dark" ? "#fff" : undefined,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <Container
+        size={600}
+        my={80}
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          background: "#fff"
+          background: "transparent",
+          color: theColorSchemeish === "dark" ? "#fff" : undefined
         }}
       >
-        <Title order={1} ta="center" mb="md" style={{ fontWeight: 800 }}>
-          HyTOP
-        </Title>
-        <Text c="dimmed" size="lg" ta="center" mb="xl">
-          Welcome to the Hyland Tech Outreach Portal. Here, you can build and
-          host your own websites.
-        </Text>
-        <Group align="center" mt="md">
-          <Button
-            size="md"
-            color={primaryColor}
-            radius="md"
-            component="a"
-            href="/create-project"
+        <Paper
+          shadow="md"
+          p={40}
+          radius="md"
+          withBorder
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            background: theColorSchemeish === "dark" ? "#23272A" : "#fff",
+            color: theColorSchemeish === "dark" ? "#fff" : undefined
+          }}
+        >
+          <Title
+            order={1}
+            ta="center"
+            mb="md"
             style={{
-              fontWeight: 600,
-              fontSize: 18,
-              paddingLeft: 32,
-              paddingRight: 32
+              fontWeight: 800,
+              color: theColorSchemeish === "dark" ? "#fff" : undefined
             }}
           >
-            Create Project
-          </Button>
-        </Group>
-      </Paper>
-    </Container>
+            HyTOP
+          </Title>
+          <Text
+            c={theColorSchemeish === "dark" ? undefined : "dimmed"}
+            size="lg"
+            ta="center"
+            mb="xl"
+            style={{
+              color: theColorSchemeish === "dark" ? "#fff" : undefined
+            }}
+          >
+            Welcome to the Hyland Tech Outreach Portal. Here, you can build and
+            host your own websites.
+          </Text>
+          <Group align="center" mt="md">
+            <Button
+              size="md"
+              color={primaryColor}
+              radius="md"
+              component="a"
+              href="/create-project"
+              style={{
+                fontWeight: 600,
+                fontSize: 18,
+                paddingLeft: 32,
+                paddingRight: 32
+              }}
+            >
+              Create Project
+            </Button>
+          </Group>
+        </Paper>
+      </Container>
+    </Box>
   );
 };
 
