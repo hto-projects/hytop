5a87388621ea3c8ec7ab7b00df15d614ff6f86e6
Joseph Maxwell <joseph.maxwell@hyland.com>
2025-08-05 15:47:26 -0400

    fix comma

--- frontend/src/screens/ProjectEditor.tsx
+++ frontend/src/screens/ProjectEditor.tsx
@@ -98,8 +98,8 @@ const DEFAULT_PANE_WIDTHS = {
   explorer: 260,
   editor: 600,
   preview: 400,
-  settings: 260
-  preferences: 260,
+  settings: 260,
+  preferences: 260
 };
 
 const MIN_PANE_WIDTH = 60;
