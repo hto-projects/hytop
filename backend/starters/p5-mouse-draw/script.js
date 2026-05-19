7134a3a2fcddb70fc8c21b88b934208a3f7f085c
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-05-19 16:39:37 -0400

    update p5 mouse draw starter

--- backend/starters/p5-mouse-draw/script.js
+++ backend/starters/p5-mouse-draw/script.js
@@ -1,15 +1,16 @@
 let img;
 
 async function setup() {
-  noCursor();
-  createCanvas(500, 500);
+  let canvas = createCanvas(500, 500);
+  canvas.elt.addEventListener("contextmenu", (e) => e.preventDefault());
+
   background(255, 255, 0);
   img = await loadImage("https://cdn.creazilla.com/cliparts/19265/cartoon-brown-mouse-clipart-xl.png");
   imageMode(CENTER);
 }
 
 function draw() {
-  if (mouseIsPressed) {
+  if (mouseButton.left) {
     image(img, mouseX, mouseY, 100, 100);
   }
 }
