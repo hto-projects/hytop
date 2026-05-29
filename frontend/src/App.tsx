01a457232710ca3089a456eebd8562d3240dd303
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-05-29 10:02:54 -0400

    maybe the last thing i will do?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,11 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Main/Header";
 import { useComputedColorScheme } from "@mantine/core";
-import { useGetProjectQuery } from "./slices/projectsApiSlice";
-import { useState } from "react";
 
 const App = () => {
   const theColorScheme = useComputedColorScheme("light");
-  const location = useLocation();
-
-  const match = location.pathname.match(/^\/([ec])\/([^/]+)$/);
-  const isEditor = !!match;
-  const projectName = match ? decodeURIComponent(match[2]) : undefined;
-  const getProject = useGetProjectQuery(projectName);
 
   return (
     <>
