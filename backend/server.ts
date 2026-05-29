2305be528a7522f6ba9e91b20c5546b33738cbc8
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-05-29 09:54:29 -0400

    revert subdomain test :(

--- backend/server.ts
+++ backend/server.ts
@@ -35,27 +35,9 @@ app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/fake-api", fakeApiRoutes);
 
-app.use(function (req: any, res: any, next: any) {
-  console.log(req.subdomains);
-  if (!req.subdomains.length || req.subdomains.slice(-1)[0] === 'www') return next();
-  // otherwise we have subdomain here
-  let subdomain = req.subdomains.slice(-1)[0];
-  // keep it
-  req.subdomain = subdomain;
-  next();
-});
-
 app.get("/up-check", (_req, res: any) => {
   res.status(200).send("<h1>BACKEND OPERATION NORMAL</h1>").end();
 });
-
-// app.get("*", (req: any, res: any, next: any) => {
-//   if (req.subdomain) {
-//     res.status(200).send(`<h1>Subdomain: ${req.subdomain}</h1>`).end();
-//   } else {
-//     next();
-//   }
-// });
 
 app.get("/pf/:projectName/:filename", renderFile);
 app.get("/pf/:projectName", renderFile);
