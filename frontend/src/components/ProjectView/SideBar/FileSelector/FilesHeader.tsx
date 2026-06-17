72f802f7c04cea705cd5e81f5f8afb6913d91532
Sophia Frey <sophia.m.frey@gmail.com>
2026-06-17 13:44:41 -0400

    fixed files spelling

--- frontend/src/components/ProjectView/SideBar/FileSelector/FilesHeader.tsx
+++ frontend/src/components/ProjectView/SideBar/FileSelector/FilesHeader.tsx
@@ -50,7 +50,7 @@ const FilesHeader = ({
     >
       <Group gap={4}>
         {SIDEBAR_ICON_MAP["Files"]}
-        <Text size="sm">Flies</Text>
+        <Text size="sm">Files</Text>
       </Group>
       {enableAddFile && (
         <ActionIcon variant="subtle" onClick={toggleAddNewFile} size="sm">
