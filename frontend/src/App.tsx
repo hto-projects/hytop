318ce3ef5e69e16dbc3415aa2a7069cccacac008
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-07-14 14:45:46 -0400

    Socket commit
    ied:   frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,41 @@ import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Main/Header";
 import { useComputedColorScheme } from "@mantine/core";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { socket } from "./socket";
+import { ConnectionState } from "./components/ConnectionState";
+import { ConnectionManager } from "./components/ConnectionManager";
+import { Events } from "./components/Events";
+import { MyForm } from "./components/MyForm";
 
 const App = () => {
   const theColorScheme = useComputedColorScheme("light");
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents((previous) => [...previous, value]);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("foo", onFooEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("foo", onFooEvent);
+    };
+  }, []);
 
   return (
     <>
@@ -16,6 +48,7 @@ const App = () => {
       />
       <Header />
       <div
+        className="App"
         id="main-container"
         style={{
           height: "calc(100vh - 48px)", // ?
@@ -23,6 +56,11 @@ const App = () => {
           flex: 1
         }}
       >
+        <ConnectionState isConnected={isConnected} />
+        <Events events={fooEvents} />
+        <ConnectionManager />
+        <MyForm />
+
         <Outlet />
       </div>
     </>
