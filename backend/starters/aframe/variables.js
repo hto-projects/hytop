5c7d83a827fb3ee02074a4fe53fe4d72b26ff74e
Sathvik Velumani <velumanisathvik@gmail.com>
2026-07-29 14:20:56 -0400

    add starter project aframe

---
+++ backend/starters/aframe/variables.js
@@ -0,0 +1,22 @@
+let cameraAcceleration = 100;
+let cameraLookControls = true;
+let cameraFly = true;
+let cameraStartX = 0;
+let cameraStartY = 5;
+let cameraStartZ = 20;
+
+
+let skyType = "gradient";
+let skyColor = "skyblue";
+let groundColor = "green";
+let seed = 2;
+let fog = .5;
+let ground = "canyon";
+let playArea = .5;
+let groundYScale = 2;
+let groundTexture = "walkernoise";
+let groundColor2 = "darkgreen";
+let dressing = "trees";
+let dressingScale = "10";
+let dressingAmount = "100";
+let dressingColor = "orange";
